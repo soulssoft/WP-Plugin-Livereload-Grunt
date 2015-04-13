6971746d528115c6493adbb0c49a53363e625b09
Romain Fischer <romain.fischer@soulsoft.com>
2015-04-13 09:58:06 +0200

    Indenting correction Description / Version correction

--- livereload_grunt.php
+++ livereload_grunt.php
@@ -6,16 +6,16 @@
 /*
 Plugin Name: LiveReload_Grunt
 Plugin URI: http://git.soulssoft.org/soulssoft/livereload_grunt
-Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
+Description: Use this plugin only in debug mode. Connect Wordpress Frontend to Grunt contrib watch - Livereload
 Author: Romain Fischer
-Version: 1.6
+Version: 1.0
 Author URI: http://www.soulssoft.fr
 */
 
 function soulssoft_livereload_grunt() {
 	if(WP_DEBUG) {
-    wp_register_script('livereload', '//' . $_SERVER['SERVER_NAME']. ':35729/livereload.js?snipver=1', null, false, true);
-    wp_enqueue_script('livereload');
-  }
+		wp_register_script('livereload', '//' . $_SERVER['SERVER_NAME']. ':35729/livereload.js?snipver=1', null, false, true);
+		wp_enqueue_script('livereload');
+	}
 }
 add_action('wp_enqueue_scripts', 'soulssoft_livereload_grunt');
